24e76ca31bf2b104687fc40f0146169c0fcb550b
Stephen Graham <stephen.graham@lethbridgecollege.ca>
2023-09-20 15:45:09 -0600

    Completed documentation and most of the simple error checking

--- checkers.cpp
+++ checkers.cpp
@@ -11,9 +11,7 @@
  *		print the grid
  *	while the user doesn't say No
  * 
- * \todo error check userChoice
- * \todo error check userRows
- * \todo add a MAXROWS
+ * \todo maybe prevent or warn users about using negative numbers
  * \todo figure out what to do when user type letter instead of number
  *********************************************************************/
 #include <iostream>
@@ -21,15 +19,29 @@
 using namespace std;
 
 int main() {
-	const int MAXCOLS = 40;
-	int userRows = 0;
-	int userCols = 0;
-	char userChoice = ' ';
+	const int MAXROWS = 25;		// upper bound for row input
+	const int MAXCOLS = 40;		// upper bound for column input
+	int userRows = 0;			// user requested number of rows
+	int userCols = 0;			// user requested number of columns
+	char userChoice = ' ';		// user's response to a question
 
+	/* print instructions for the user */
+	cout << "This program prints a 'checkerboard' pattern of +'s and o's" << endl;
+	cout << "You get to decide (within limits) how many rows and columns." << endl;
+
+	/* repeat the checkerboard generator until the user wants to stop */
 	do {
-		cout << "How many rows? ";
-		cin >> userRows;
 
+		/* get the number of rows from the user */
+		do {
+			cout << "How many rows? ";
+			cin >> userRows;
+			if (userRows > MAXROWS) {
+				cout << "I can't do that, Dave. It cant be more than " << MAXROWS << "." << endl;
+			}
+		} while (userRows > MAXROWS);
+
+		/* get the nubmer of columns from the user */
 		do {
 			cout << "How many columns? ";
 			cin >> userCols;
@@ -38,36 +50,60 @@ int main() {
 			}
 		} while (userCols > MAXCOLS);
 
+		/* build the checkerboard */
 		for (int row = 1; row <= userRows; ++row) {
+
 			for (int column = 1; column <= userCols; ++column) {
 
 				/* for each odd row*/
 				if (row % 2 == 1) {
-					if (column % 2 == 1) { //is odd
+					if (column % 2 == 1) {
+						// odd row + odd column
 						cout << '+';
 					}
-					else { // is even
+					else {
+						// odd row + even column
 						cout << 'o';
 					}
 				}
 				/* or for each even row */
 				else {
-					if (column % 2 == 1) { //is odd
+					if (column % 2 == 1) { 
+						// even row + odd column
 						cout << 'o';
 					}
-					else { // is even
+					else {
+						// even row + even column
 						cout << '+';
 					}
 				}
 
-			} // end of the column loop
+			} // end of the column loop -- time to go to the next row
 			cout << endl;
-		} // end of the row loop
-	
-		cout << "Print another grid (Y/N)? ";
-		cin >> userChoice;
 
-	} while (userChoice != 'N' && userChoice != 'n');
+		} // end of the row loop - if there are no more rows we are done this grid
+	
+		/* get a valid choice from the user */
+		do {
+			cout << "Print another grid (Y/N)? ";
+			cin >> userChoice;
+
+			/* Adjust the choice to 'N' or 'Y' for valid choices */
+			switch (userChoice) {
+			case 'N':
+			case 'n':
+				userChoice = 'N'; break;
+			case 'Y':
+			case 'y':
+				userChoice = 'Y'; break;
+			default:
+				userChoice = ' ';
+				cout << "That is not a valid choice. Please try again." << endl;
+			}
+
+		} while (userChoice != 'N' && userChoice != 'Y');
+
+	} while (userChoice != 'N');
 
 	return 0;
 }
